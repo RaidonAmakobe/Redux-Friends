09aa8288960bac31f126b91a7c7effd2bda6a5ad
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-16 18:55:54 -0400

    declared initial state first reducer

--- friends/src/reducer/firstReducer.js
+++ friends/src/reducer/firstReducer.js
@@ -1,1 +1,14 @@
 import { FETCHING, SUCCESS, FAILURE } from '.../actions';
+
+const initialState = {
+
+    deletingFriend: false,
+    fetchingFriends: false,
+    fetchedFriend: false,
+    friends: [],
+    loggingIn: false,
+    savingFriends: false,
+    savedFriend: false,
+    updatingFriend: false,
+    error: null
+};
